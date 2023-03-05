feaba0fbd43f66ea3a8998859aef9891772718f5
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-03-05 23:42:30 +0300

    Malignant and Benign components added

---
+++ user_interface/user_interface/src/app/malignant/malignant.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-malignant',
+  templateUrl: './malignant.component.html',
+  styleUrls: ['./malignant.component.css']
+})
+export class MalignantComponent {
+
+}
